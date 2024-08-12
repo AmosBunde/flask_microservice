ef28bc62407b8a3452923c0d4536f629f4252aaa
AmosBunde <git.bunde@hotmail.com>
2024-08-12 09:06:38 +0300

    User database model and routes

--- user/app.py
+++ user/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import model
 from routes import user_blueprint
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'aLLee3vF_ofXyZboEJy_giwpp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+model.init_app(app)
 
 app.register_blueprint(user_blueprint)
 
